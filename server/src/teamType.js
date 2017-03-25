2e9832702b94ad50cf248a88c6861905a6cf9258
Laney Kuenzel <laney@fb.com>
2017-03-25 13:37:22 -0400

    Add random team data

--- server/src/teamType.js
+++ server/src/teamType.js
@@ -5,6 +5,7 @@ import {
   GraphQLList,
 } from 'graphql';
 import {teams, locations} from './db';
+import {teamData} from './teamData';
 import locationType from './locationType';
 
 const teamType = new GraphQLObjectType({
@@ -22,8 +23,43 @@ const teamType = new GraphQLObjectType({
         );
         return teamLocations;
       }
-    }
+    },
+    venue: {
+      type: GraphQLString,
+      resolve: (team) => {
+        const teamDataEntry = getTeamData(team);
+        return teamDataEntry ? teamDataEntry.venue_name : null;
+      }
+    },
+    team_url: {
+      type: GraphQLString,
+      resolve: (team) => {
+        const teamDataEntry = getTeamData(team);
+        return teamDataEntry ? teamDataEntry.base_url : null;
+      }
+    },
+    first_year_of_play: { 
+      type: GraphQLString,
+      resolve: (team) => {
+        const teamDataEntry = getTeamData(team);
+        return teamDataEntry ? teamDataEntry.first_year_of_play : null;
+      }
+    },
+    division: { 
+      type: GraphQLString,
+      resolve: (team) => {
+        const teamDataEntry = getTeamData(team);
+        return teamDataEntry ? teamDataEntry.division_full : null;
+      }
+    },
   }),
 });
+
+function getTeamData(team) {
+  return teamData.team_all.queryResults.row.find(
+    row => row.name_display_full === team.name || row.name_abbrev === team.id
+  );
+}
+
 
 export default teamType;
